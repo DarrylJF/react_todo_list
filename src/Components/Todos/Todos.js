9b88dddb22888d07c106214015a71ad21a31776e
DarrylJF <33667200+DarrylJF@users.noreply.github.com>
2020-08-26 15:04:08 +0100

    added todos component to host todo instead of directly rendering each todo in the app component, a single todos (list) in rendered

--- src/Components/Todos/Todos.js
+++ src/Components/Todos/Todos.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Todo from './Todo/Todo'
 
 const Todos = ( props ) => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return props.todos
+    .filter(props.filter_map[props.filter])
+    .map(todo => {
+      return (
+        <Todo 
+        id={todo.id}
+        name={todo.name}
+        completed={todo.completed}
+        key={todo.id}
+        deleteTodo={props.deleteTodo}
+        editTodo={props.editTodo}
+        toggleCompleted={props.toggleCompleted}
+        />
+      );
+    });
+  };
 
 export default Todos;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './App.scss';
 import Cockpit from '../components/Cockpit/Cockpit';
-import Todo from '../components/Todos/Todo/Todo';
+import Todos from '../components/Todos/Todos';
 import FilterButton from '../components/FilterButton/FilterButton';
 import { nanoid } from 'nanoid';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -27,6 +27,7 @@ const App = ( props ) => {
   const addTodoHandler = ( name ) => {
     const newTodo = {id: 'todo-' + nanoid(), name: name, completed: false};
     setTodos([...todos, newTodo]);
+    setShowTodos(true);
   }
 
   const deleteTodoHandler = ( id ) => {
@@ -56,20 +57,16 @@ const App = ( props ) => {
  
   let todoList = [];
   if ( showTodos ) {
-    todoList = todos
-    .filter(FILTER_MAP[filter])
-    .map(todo => (
-      <Todo 
-        id={todo.id}
-        name={todo.name}
-        completed={todo.completed}
-        key={todo.id}
-        deleteTodo={deleteTodoHandler}
-        editTodo={editTodoHandler}
-        toggleCompleted={toggleCompletedHandler}
-        />
-  ));
+    todoList = <Todos
+      todos={todos}
+      deleteTodo={deleteTodoHandler}
+      editTodo={editTodoHandler}
+      toggleCompleted={toggleCompletedHandler}
+      filter_map={FILTER_MAP}
+      filter={filter}
+    />
   }
+  
   
   const filterList = FILTER_NAMES.map( name => (
     <FilterButton 
@@ -92,9 +89,7 @@ const App = ( props ) => {
         filterList={filterList}
         addTodo={addTodoHandler}
         toggleTodos={toggleTodosHandler}/>
-        <ul>
-          {todoList}
-        </ul>
+        {todoList}
       </div>
   )
 }
